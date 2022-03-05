308d39229a478181923e5543022db374ac91fc88
atharvdavle <91043766+atharvdavle@users.noreply.github.com>
2022-03-05 17:48:05 +0530

    Add files via upload

---
+++ MARKSHEET.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+  int roll,name,maths,c,ele,graphics,cs,i;
+  
+  printf("Enter roll your roll no.=");
+  scanf("%d",&roll);
+  for(i=1;i<=80;i++)
+  {
+    printf("=");
+  }
+  
+  printf("\n                  SVERI'S Collage of Engineering,Pandhrpur");
+  printf("\n");
+   for(i=1;i<=80;i++)
+  {
+    printf("=");
+  }
+  printf("\n                          ||FY BTECH SCORE CARD||");
+  printf("\n");
+  for(i=1;i<=80;i++)
+  {
+    printf("=");
+  }
+  
+}
